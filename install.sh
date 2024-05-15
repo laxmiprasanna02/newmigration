bed36de78e9e433c2282bbe73448f8b9a1754ce8
laxmiprasanna02 <121659087+laxmiprasanna02@users.noreply.github.com>
2024-05-15 17:26:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 set -e
 
 # Install Node.js
-sudo yum install nodejs -y
+sudo apt-get update
+sudo apt-get install -y nodejs npm
 
 # Install build tools
-sudo yum install -y gcc gcc-c++
+sudo apt-get install -y build-essential
 
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -21,20 +22,20 @@ npm install
 npm install ts-node --legacy-peer-deps
 
 # Terraform installation
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository --yes --update https://apt.releases.hashicorp.com
+sudo apt-get install -y terraform
 
 # Set AWS environment variables
 export AWS_REGION=eu-west-1
 export AWS_PROFILE=dev
 export TF_VAR_profile=dev
 
-# Navigate to Terraform directory
+# Navigate to the Terraform directory
 # cd Terraform
 
 # # Initialize Terraform
-# terraform init -backend-config="bucket=eu-eu-west-1-dev-156696388136-terraform-state-backend-187" -reconfigure
+# terraform init -backend-config="bucket=eu-eu-west-1-dev-156696388136-terraform-state-backend-187" -force-copy
 
 # # Apply Terraform changes
 # terraform apply --auto-approve
