832669edff536f7fa03c5a0ed67369b9ab5a48c1
laxmiprasanna02 <121659087+laxmiprasanna02@users.noreply.github.com>
2024-05-15 16:59:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e
 
-# Update the system and install necessary packages
+# Prepare the system
 sudo apt-get update
-sudo apt-get install -y nodejs npm curl software-properties-common
+sudo apt-get install -y curl software-properties-common
 
-# Install gcc and g++
-sudo apt-get install -y gcc g++
+# Add HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 
-# Install nvm and use it to install Node.js
+# Add HashiCorp repository
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+# Update the system and install Terraform
+sudo apt-get update
+sudo apt-get install -y terraform
+
+# Install Node.js using nvm for better version management
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -19,17 +26,12 @@ nvm use 18
 npm install
 npm install ts-node --legacy-peer-deps
 
-# Terraform installation
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install -y terraform
-
-# Environment variables for Terraform
+# Setup environment variables for Terraform
 export AWS_REGION=eu-west-1
 export AWS_PROFILE=dev
 export TF_VAR_profile=dev
 
-# Initialize and apply Terraform configuration
+# Assume the Terraform directory is correctly setup in the repository
 cd Terraform
 terraform init -backend-config="bucket=eu-west-1-dev-156696388136-terraform-state-backend-187" -force-copy
-terraform apply --auto-approve
+terraform apply -auto-approve
