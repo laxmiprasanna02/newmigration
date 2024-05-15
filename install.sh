363563ec0314c8a41e8d31a5c12b385bf86f1c04
laxmiprasanna02 <121659087+laxmiprasanna02@users.noreply.github.com>
2024-05-15 16:55:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 set -e
 
-sudo yum install nodejs -y
-sudo yum install gcc -y  && sudo yum install gcc-c++ -y  && curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.bashrc && export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm install 18 && nvm use 18
+# Update the system and install necessary packages
+sudo apt-get update
+sudo apt-get install -y nodejs npm curl software-properties-common
 
+# Install gcc and g++
+sudo apt-get install -y gcc g++
+
+# Install nvm and use it to install Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 18
+nvm use 18
+
+# Install npm packages
 npm install
 npm install ts-node --legacy-peer-deps
 
 # Terraform installation
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
-# This script is to maintain AWS resources created by terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install -y terraform
+
+# Environment variables for Terraform
 export AWS_REGION=eu-west-1
 export AWS_PROFILE=dev
 export TF_VAR_profile=dev
+
+# Initialize and apply Terraform configuration
 cd Terraform
-terraform init -backend-config="bucket=eu-eu-west-1-dev-156696388136-terraform-state-backend-187" -force-copy
+terraform init -backend-config="bucket=eu-west-1-dev-156696388136-terraform-state-backend-187" -force-copy
 terraform apply --auto-approve
