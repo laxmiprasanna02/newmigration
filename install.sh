7bbb3d8c81a99a13bdb2f10aa44539916bda665b
laxmiprasanna02 <121659087+laxmiprasanna02@users.noreply.github.com>
2024-05-16 16:01:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
-# Ensure line endings are Unix format for sourced files (if any)
-# dos2unix <list of files>
-
-# Update package lists and install necessary packages
+# Ensure curl and lsb-release are installed
 sudo apt-get update
-sudo apt-get install -y nodejs npm build-essential
+sudo apt-get install -y curl lsb-release dos2unix nodejs npm build-essential
 
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -32,3 +29,13 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://
 # Update package lists again and install Terraform
 sudo apt-get update
 sudo apt-get install -y terraform
+
+# Set environmental variables for AWS and Terraform
+export AWS_REGION=eu-west-1
+export AWS_PROFILE=dev
+export TF_VAR_profile=dev
+
+# Initialize and apply Terraform configuration
+cd Terraform
+terraform init -backend-config="bucket=eu-west-1-dev-156696388136-terraform-state-backend" -force-copy
+terraform apply --auto-approve
