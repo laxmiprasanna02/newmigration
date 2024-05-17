63c4acb5c12fc9cf36cc47e06812881ec93903a4
laxmiprasanna02 <121659087+laxmiprasanna02@users.noreply.github.com>
2024-05-17 17:18:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e  # Exit on first error
+set -e  # Exit on error
 
 # Update package lists
 sudo apt update
@@ -9,33 +9,31 @@ sudo apt install -y nodejs gcc g++
 
 # Install and configure nvm (Node Version Manager)
 curl -sSL https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
+source ~/.bashrc  # Reload bashrc to ensure nvm is loaded
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Use backslash escape for source in this line
-nvm install 18
+nvm install 18  # Install Node.js v18
 nvm use 18
 
 # Install npm packages
 npm install
 npm install ts-node --legacy-peer-deps
 
-# Install Terraform (using HashiCorp repository)
-sudo apt update  # Update package lists again (needed after adding repository)
-# Add the HashiCorp GPG key (assuming you haven't already)
+# Terraform installation via HashiCorp's official repository
+sudo apt-get update && sudo apt-get install -y software-properties-common gnupg
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-# Add the HashiCorp repository
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list >/dev/null
-# Install Terraform
+sudo apt update
 sudo apt install -y terraform
 
 # Set environment variables for AWS and Terraform
-export AWS_REGION=eu-west-1
-export AWS_PROFILE=dev
-export TF_VAR_profile=dev
+export AWS_REGION="eu-west-1"
+export AWS_PROFILE="dev"
+export TF_VAR_profile="dev"
 
-# Change directory to Terraform project (adjust if needed)
+# Change directory to Terraform project (adjust directory as needed)
 cd Terraform
 
-# Terraform commands (replace with your specific commands)
-terraform init -backend-config="bucket=eu-west-1-dev-156696388136-terraform-state-backend-187" -force-copy
-terraform apply --auto-approve
+# Terraform initialization and apply
+# terraform init -backend-config="bucket=eu-west-1-dev-156696388136-terraform-state-backend-187" -force-copy
+# terraform apply --auto-approve
